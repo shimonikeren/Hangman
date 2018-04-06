ea03c32d60ed3d27d1c4f5ab6426d7cbc113a20f
Keren Shimoni <kerenshimoni6@gmail.com>
2018-04-06 16:21:13 -0700

    word generator

--- Word.js
+++ Word.js
@@ -1,10 +1,23 @@
 //require Letter file, containing letter constrector 
 var word = require("./Letter.js");
 
-var Word = {
-    //Word constructor 
+
+
+
+var Word = function(number) {
+    var wordBank = ["cookies", "puppies", "elephant", "deskchair", "flowers"];
+    var wordGenerator = wordBank[Math.floor(Math.random() * wordBank.length)];
+    this.chosenWord = wordGenerator;
+        console.log(this.chosenWord);
+    var splitWord = chosenWord.split("");
+    this.letterArray = splitWord;
+    console.log(letterArray);
+    console.log("FIRST LETTER IN WORD: " + letterArray[0]);
 }
 
+
+
+Word();
 
 //export word constructor 
 module.exports = Word;
